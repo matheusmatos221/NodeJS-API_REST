342da1f2a7a78e91871d4d97a48b42a3fd930b1d
Matheus Matos <matheus.matos@accurate.com.br>
2022-01-21 11:32:05 -0300

    ADD class to handle with contentType

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const config = require('config')
 const NaoEncontrado = require('./erros/NaoEncontrado')
 const CampoInvalido = require('./erros/CampoInvalido')
 const DadosNaoFornecidos = require('./erros/DadosNaoFornecidos')
+const ValorNaoSuportado = require('./erros/ValorNaoSuportado')
 
 app.use(bodyParser.json())
 
@@ -19,6 +20,9 @@ app.use((erro, requisicao, resposta, proximo) => {
     }
     if (erro instanceof CampoInvalido || erro instanceof DadosNaoFornecidos) {
         status = 400
+    }
+    if (erro instanceof ValorNaoSuportado) {
+        status = 406
     }
     resposta.status(status)
      resposta.send(

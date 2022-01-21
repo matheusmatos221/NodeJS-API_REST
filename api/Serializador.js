cc06c6f63a7df6cf3f2c4500ef8169fd1ce9a4c8
Matheus Matos <matheus.matos@accurate.com.br>
2022-01-21 11:58:53 -0300

    Exports Serializador and ADD a midleware to validate typeContent

--- api/Serializador.js
+++ api/Serializador.js
@@ -11,3 +11,8 @@ class Serializador {
         throw new ValorNaoSuportado(this.contentType)
     }   
 }
+
+module.exports = {
+    Serializador: Serializador,
+    formatosAceitos: ['application/json']
+}

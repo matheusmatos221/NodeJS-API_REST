69010771f90d5703e44ef67ac9678706124fd0a7
Matheus Matos <matheus.matos@accurate.com.br>
2022-01-24 10:17:57 -0300

    ADD support to XML

--- api/Serializador.js
+++ api/Serializador.js
@@ -1,14 +1,30 @@
 const ValorNaoSuportado = require("./erros/ValorNaoSuportado")
+const jsontoxml = require('jsontoxml')
 
 class Serializador {
     json(dados) {
         return JSON.stringify(dados)
     }
+    xml(dados) {
+        let tag = this.tagSingular
+
+        if (Array.isArray(dados)) {
+            tag = this.tagPlural
+            dados = dados.map( (item) => {
+                return {
+                    [this.tagSingular]: item
+                }
+            })
+        }
+        return jsontoxml({[tag]: dados})
+    }
     serializar(dados) {
+        dados = this.filtrar(dados)
         if (this.contentType === 'application/json') {
-            return this.json(
-                this.filtrar(dados)
-            )
+            return this.json(dados)
+        }
+        if (this.contentType === 'application/xml') {
+            return this.xml(dados)
         }
         throw new ValorNaoSuportado(this.contentType)
     }
@@ -39,6 +55,8 @@ class SerializadorFornecedor extends Serializador {
         super()
         this.contentType = contentType
         this.camposPublicos = ['id', 'empresa', 'categoria'].concat(camposExtras || [])
+        this.tagSingular = 'fornecedor'
+        this.tagPlural = 'fornecedores'
     }
 }
 
@@ -47,6 +65,8 @@ class SerializadorErro extends Serializador {
         super()
         this.contentType = contentType
         this.camposPublicos = ['id', 'mensagem'].concat(camposExtras || [])
+        this.tagSingular = 'erro'
+        this.tagPlural = 'erros'
     }
 }
 
@@ -54,5 +74,5 @@ module.exports = {
     Serializador: Serializador,
     SerializadorFornecedor: SerializadorFornecedor,
     SerializadorErro: SerializadorErro,
-    formatosAceitos: ['application/json']
+    formatosAceitos: ['application/json', 'application/xml']
 }
